c4fbb66db7222fe5ce51489b3a8621dd1eb743c0
Sebastian lopez D <sebastian.lopezd@udea.edu.co>
2022-06-19 23:13:37 -0500

    Con carpetas de ViewModel y LiveData ResistCalculator

--- app/src/main/java/com/lopezing/resistapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/lopezing/resistapp/ui/main/MainActivity.kt
@@ -14,11 +14,13 @@ import com.lopezing.resistapp.databinding.ActivityMainBinding
 import com.lopezing.resistapp.ui.ajust.AjustActivity
 import kotlin.math.pow
 private lateinit var mainBinding: ActivityMainBinding
+private lateinit var mainViewModel: MainViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var mainViewModel: MainViewModel
     private var ban1: Double=0.0
     private var lin1=0
     private var col=10
+    private var colod1=1
     private var ban2: Double=0.0
     private var ban3: Double=0.0
     private var sumt: Double=0.0
@@ -26,49 +28,7 @@ class MainActivity : AppCompatActivity() {
     private fun most() {
         sumt = (((ban1 * 10) + (ban2) )* (10.0.pow(ban3)))
         sumt1=sumt.toInt()
-        mainBinding.textV.text = sumt1.toString()+"Ω "+"  ±"+col.toString()+" %"
-    }
-    private  fun Acol3(){
-        when(lin1){
-            0 -> mainBinding.button3.setBackgroundResource(R.color.black)
-            1 -> mainBinding.button3.setBackgroundResource(R.color.maroon)
-            2 -> mainBinding.button3.setBackgroundResource(R.color.red)
-            3 -> mainBinding.button3.setBackgroundResource(R.color.orange)
-            4 -> mainBinding.button3.setBackgroundResource(R.color.yellow)
-            5 -> mainBinding.button3.setBackgroundResource(R.color.lime)
-            6 -> mainBinding.button3.setBackgroundResource(R.color.blue)
-            7 -> mainBinding.button3.setBackgroundResource(R.color.purple)
-            8 -> mainBinding.button3.setBackgroundResource(R.color.gray)
-            9 -> mainBinding.button3.setBackgroundResource(R.color.silver)
-        }
-    }
-    private  fun Acol2(){
-        when(lin1){
-            0 -> mainBinding.button2.setBackgroundResource(R.color.black)
-            1 -> mainBinding.button2.setBackgroundResource(R.color.maroon)
-            2 -> mainBinding.button2.setBackgroundResource(R.color.red)
-            3 -> mainBinding.button2.setBackgroundResource(R.color.orange)
-            4 -> mainBinding.button2.setBackgroundResource(R.color.yellow)
-            5 -> mainBinding.button2.setBackgroundResource(R.color.lime)
-            6 -> mainBinding.button2.setBackgroundResource(R.color.blue)
-            7 -> mainBinding.button2.setBackgroundResource(R.color.purple)
-            8 -> mainBinding.button2.setBackgroundResource(R.color.gray)
-            9 -> mainBinding.button2.setBackgroundResource(R.color.silver)
-        }
-    }
-    private  fun Acol1(){
-        when(lin1){
-            0 -> mainBinding.button1.setBackgroundResource(R.color.black)
-            1 -> mainBinding.button1.setBackgroundResource(R.color.maroon)
-            2 -> mainBinding.button1.setBackgroundResource(R.color.red)
-            3 -> mainBinding.button1.setBackgroundResource(R.color.orange)
-            4 -> mainBinding.button1.setBackgroundResource(R.color.yellow)
-            5 -> mainBinding.button1.setBackgroundResource(R.color.lime)
-            6 -> mainBinding.button1.setBackgroundResource(R.color.blue)
-            7 -> mainBinding.button1.setBackgroundResource(R.color.purple)
-            8 -> mainBinding.button1.setBackgroundResource(R.color.gray)
-            9 -> mainBinding.button1.setBackgroundResource(R.color.silver)
-        }
+
     }
 
 
@@ -78,6 +38,12 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view=mainBinding.root
         mainViewModel= ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel.colordata.observe(this) { colod ->
+            colod1=colod
+        }
+        mainViewModel.sumtdata.observe(this){tot->
+            mainBinding.textV.text =tot
+        }
         setContentView(view)
 
         with(mainBinding) {
@@ -112,37 +78,23 @@ class MainActivity : AppCompatActivity() {
                 lin1=9
             }
             button1.setOnClickListener {
-                ban1 = lin1.toDouble()
-                if(lin1==0) {
-                    button1.setTextColor(resources.getColor(R.color.lime))
-                }else button1.setTextColor(resources.getColor(R.color.black))
-                most()
-                Acol1()
-
+                //ban1 = lin1.toDouble()
+                mainViewModel.coloA(lin1,1,col)
+                mainBinding.button1.setBackgroundResource(colod1)
             }
             button2.setOnClickListener {
-                ban2 = lin1.toDouble()
-                if(lin1==0) {
-                    button2.setTextColor(resources.getColor(R.color.lime))
-                }else button2.setTextColor(resources.getColor(R.color.black))
-                most()
-                Acol2()
+                mainViewModel.coloA(lin1,2,col)
+                mainBinding.button2.setBackgroundResource(colod1)
             }
             button3.setOnClickListener {
-                ban3 = lin1.toDouble()
-                if(lin1==0) {
-                    button3.setTextColor(getResources().getColor(R.color.lime))
-                }else button3.setTextColor(getResources().getColor(R.color.black))
-                most()
-                Acol3()
+                mainViewModel.coloA(lin1,3,col)
+                mainBinding.button3.setBackgroundResource(colod1)
             }
             buttonP.setOnClickListener {
                 col=10
-                most()
             }
             buttonD.setOnClickListener {
                 col=5
-                most()
             }
         }
     }
--- app/src/main/java/com/lopezing/resistapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/lopezing/resistapp/ui/main/MainViewModel.kt
@@ -1,6 +1,66 @@
 package com.lopezing.resistapp.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lopezing.resistapp.R
+import kotlin.math.pow
 
 class MainViewModel : ViewModel( ) {
+    private val sumt1: MutableLiveData<String> = MutableLiveData()
+    val sumtdata: LiveData<String> = sumt1
+    private val coloritems: MutableLiveData<Int> = MutableLiveData()
+    val colordata: LiveData<Int> = coloritems
+    private var coloritem=0
+    private var ban1=0
+    private var ban2=0
+    private var ban3=0
+    private var sumt=0.0
+    private var z1=10
+    fun coloA(x:Int,y:Int,z:Int){
+        when(x){
+            0 -> {
+                coloritem=R.color.black
+            }
+            1 -> {coloritem=R.color.maroon
+            }
+            2 -> {
+                coloritem=R.color.red
+            }
+            3 -> {coloritem=R.color.orange
+            }
+            4 -> {coloritem=R.color.yellow
+            }
+            5 -> {coloritem=R.color.lime
+            }
+            6 -> {coloritem=R.color.blue
+            }
+            7 -> {coloritem=R.color.purple
+            }
+            8 -> {coloritem=R.color.gray
+            }
+            9 -> {coloritem=R.color.silver
+            }
+            else -> {R.color.white}
+        }
+        coloritems.value=coloritem
+        when(y){
+            1->ban1=x
+            2->ban2=x
+            3->ban3=x
+        }
+        z1=z
+        most()
+
+    }
+    fun most() {
+        sumt = (((ban1 * 10) + (ban2) )* (10.0.pow(ban3)))
+        if (sumt<1000){
+            sumt1.value=sumt.toString()+"Ω "+"  ±"+z1.toString()+" %"
+        }else{
+            sumt/=1000
+            sumt1.value=sumt.toString()+"KΩ "+"  ±"+z1.toString()+" %"
+        }
+
+    }
 }
